22d5ddbc75db2342e8508b17e49e6e86fee4944c
Gustavo <guuu.developer@gmail.com>
2022-09-29 16:34:46 -0300

    repository and controller of product has been created

--- GeekShopping/GeekShopping.ProductAPI/Startup.cs
+++ GeekShopping/GeekShopping.ProductAPI/Startup.cs
@@ -1,17 +1,15 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GeekShopping.ProductAPI
 {
@@ -33,6 +31,12 @@ namespace GeekShopping.ProductAPI
                 UseMySql(connection,
                         new MySqlServerVersion(
                             new Version(8, 0, 5))));
+
+            IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+            services.AddSingleton(mapper);
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddScoped<IProductRepository, ProductRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
